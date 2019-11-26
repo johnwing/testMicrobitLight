975adf2c58aa25075fb72d35fe5a495957866bf7
johnwing <47427803+johnwing@users.noreply.github.com>
2019-11-27 01:54:48 +0800

    Update gatorlight.cpp

--- gatorlight.cpp
+++ gatorlight.cpp
@@ -25,9 +25,8 @@ namespace gatorlight {
     * Calculates the light in Lux based on the ADC value passed in. 1 step in adcVal is equal to .488 uA or .976 lux at 5V
     */
     //%
-    uint16_t getLux(int16_t ADCVal) {
-
-        return ADCVal * 2;
+    float getMoisture(int16_t ADCVal) {
+        return ADCVal / 1023.0;
     }
 
 }
